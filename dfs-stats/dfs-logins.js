87c1801502da8940e382bd0563f1ae07f48a26f1
Andrew Seales <andrew.seales@ed.ac.uk>
2013-07-01 15:58:54 +0000

    Add tiles total to DfS.

--- dfs-stats/dfs-logins.js
+++ dfs-stats/dfs-logins.js
@@ -5,7 +5,8 @@
     prints: createTimeSeries( 'Prints', ['digimapforschools.web.prints_count'], '#prints' ),
     tiles: createTimeSeries( 'Tiles', ['digimapforschools.web.mapproxies_count'], '#tiles' ),
     bookmarks: createTimeSeries( 'Bookmarks', ['digimapforschools.web.bookmarks_load_count', 'digimapforschools.web.bookmarks_save_count'], '#bookmarks', 2 ),
-    'total_logins': createGaugeLabel( 'Total Logins in Last Hour', 'integral(digimapforschools.web.logins_count)', '#logins-count', 'max' )
+    'total_logins': createGaugeLabel( 'Total Logins in Last Hour', 'integral(digimapforschools.web.logins_count)', '#logins-count', 'max' ),
+    'total_tiles': createGaugeLabel( 'Total Tiles in Last Hour', 'integral(digimapforschools.web.mapproxies_count)', '#tiles-count', 'max' )
   };
 
 
